1ad8e5d544c7bc336727c84e44f27608a19b44fa
Cory Duplantis <cld251@gmail.com>
2024-08-01 16:44:00 -0500

    Attempt to not create an overlapping chain when connecting pieces together

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ fn main() {
     let r2 = circuit.resistor(200);
     let r3 = circuit.resistor(500);
 
-    circuit.connect(&[motor, r1]);
-    circuit.connect(&[motor, r2, r3]);
+    circuit.connect(&[motor, r3, r2, r1]);
 
     circuit.save("/home/user/test.spin");
 }
